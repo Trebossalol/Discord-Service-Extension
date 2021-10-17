4f70247605ccf19ee6c8b214b32332cabaca4c45
Trebossa BT <48384398+Trebossalol@users.noreply.github.com>
2021-10-17 23:38:15 +0200

    Add files via upload

---
+++ plugins/block.js
@@ -0,0 +1,15 @@
+let lac90rucn32023c = ''
+
+// Remove everything document
+const eijcf8u02ß240utc = () => {
+  cache = document.documentElement.innerHTML
+  document.documentElement.innerHTML = ''
+}
+
+// Restore everything
+const ct3h4w8tnczp2rx = () => {
+  document.documentElement.innerHTML = cache
+}
+
+// Remove everything after 1 second
+setTimeout(eijcf8u02ß240utc, 1000)
