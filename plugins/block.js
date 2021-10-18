f34aa141cc851f2664c93d76a8e73d75ba912874
Trebossa BT <48384398+Trebossalol@users.noreply.github.com>
2021-10-18 05:52:22 +0200

    Update block.js

--- plugins/block.js
+++ plugins/block.js
@@ -2,13 +2,13 @@ let lac90rucn32023c = ''
 
 // Remove everything document
 const eijcf8u02ß240utc = () => {
-  cache = document.documentElement.innerHTML
+  lac90rucn32023c = document.documentElement.innerHTML
   document.documentElement.innerHTML = ''
 }
 
 // Restore everything
 const ct3h4w8tnczp2rx = () => {
-  document.documentElement.innerHTML = cache
+  document.documentElement.innerHTML = lac90rucn32023c
 }
 
 // Remove everything after 1 second
